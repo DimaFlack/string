429b526b8f0add25bdaff938d490160bd6e16c8d
DimaFlack <57666659+DimaFlack@users.noreply.github.com>
2019-11-12 14:45:46 +0300

    Add files via upload

---
+++ ПР9(9)/СР(2)/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace СР_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Надо сыграть в кампухтер, но нет времени"; 
+            Console.WriteLine(s.Substring(26)); 
+            Console.WriteLine(s.Substring(26)); 
+            Console.WriteLine(s.Substring(26)); 
+            Console.ReadKey();
+        }
+    }
+}
