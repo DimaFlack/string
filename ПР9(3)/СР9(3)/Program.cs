174d6afef2d894552141e8c607b5de3660cbd5ce
DimaFlack <57666659+DimaFlack@users.noreply.github.com>
2019-11-12 14:41:42 +0300

    Add files via upload

---
+++ ПР9(3)/СР9(3)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace СР9_3_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputLine = string.Empty;
+            int count = 0;
+            Console.Write("Input string line: ");
+            inputLine = Console.ReadLine();
+            count = inputLine.Length;
+            Console.WriteLine("First: {0}, Last {1}", inputLine[0].ToString(), inputLine[count - 1]);
+            Console.ReadKey();
+        }
+    }
+}
