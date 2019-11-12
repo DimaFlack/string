20c92f0398b503003294cff0b5736e29acbbb6a0
DimaFlack <57666659+DimaFlack@users.noreply.github.com>
2019-11-12 14:46:31 +0300

    Add files via upload

---
+++ ПР9(10)/СР9(10)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace СР9_10_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "abc, www"; 
+            Console.WriteLine(s.Replace("abc", "www")); 
+            Console.ReadLine();
+        }
+    }
+}
