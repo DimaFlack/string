8f65fde49b20b3c8f1c5cac12bb13468ef63490b
DimaFlack <57666659+DimaFlack@users.noreply.github.com>
2019-11-12 14:47:47 +0300

    Add files via upload

---
+++ ПР9(12)/СР9(12)/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace СР9_12_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "f2f34l43o9O03f";
+            char[] ch = s.ToCharArray();
+            var count = ch.Where((n) => n >= '0' && n <= '9').Count();
+            Console.WriteLine("Кол-во цифр в строке:" + count);
+            Console.ReadKey();
+        }
+    }
+}
