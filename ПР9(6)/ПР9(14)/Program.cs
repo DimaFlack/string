d933e2a5ba0993e22c1a5bff1aa606b8005f8f5f
DimaFlack <57666659+DimaFlack@users.noreply.github.com>
2019-11-12 14:44:23 +0300

    Add files via upload

---
+++ ПР9(6)/ПР9(14)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ПР9_14_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Convert.ToString(Console.ReadLine());
+            for (int i = 0; i < s.Length - 1; i += 3)
+            {
+
+                Console.Write(s[i] + ",");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
