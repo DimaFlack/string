551c2450b65e1d98e57cac83eab578ddce60fe3d
DimaFlack <57666659+DimaFlack@users.noreply.github.com>
2019-11-12 14:49:25 +0300

    Add files via upload

---
+++ ПР9(15)/ПР9(15)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ПР9_15_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine().Replace("xabc", "abc");
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
