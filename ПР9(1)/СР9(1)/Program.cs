b10ab7442fae2d458df74bf274304f1c91f2d8a5
DimaFlack <57666659+DimaFlack@users.noreply.github.com>
2019-11-12 14:38:29 +0300

    Add files via upload

---
+++ ПР9(1)/СР9(1)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace СР9_1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Nikolay, Oleg"; 
+            Console.WriteLine(s.Replace("Nikolay", "Oleg")); 
+            Console.ReadLine();
+        }
+    }
+}
