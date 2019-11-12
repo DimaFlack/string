a3b4880742040822a6d2cd5684103700cf2650b0
DimaFlack <57666659+DimaFlack@users.noreply.github.com>
2019-11-12 14:50:00 +0300

    Add files via upload

---
+++ ПР9(16)/СР9(16)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace СР9_16_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Сегодня мы с вами рассмотрели, как работать со строками в Си-шарп. Были описаны основные операторы и методы, которые используются для работы со строками";
+            Console.WriteLine(s.Substring(67, 40));
+            Console.ReadLine();
+        }
+    }
+}
