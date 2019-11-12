57c74c1930d069324eebeccb85512acd41b9e86d
DimaFlack <57666659+DimaFlack@users.noreply.github.com>
2019-11-12 14:47:11 +0300

    Add files via upload

---
+++ ПР9(11)/СР9(11)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace СР9_11_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Алоооооооо";
+            Console.WriteLine(s.Substring(0, 6));
+            Console.ReadLine();
+        }
+    }
+}
